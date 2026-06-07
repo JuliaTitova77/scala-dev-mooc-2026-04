c1392937bac2d77eb528a310dfac486c0a179917
Julia Titova <julia.titova-2015@yandex.ru>
2026-06-07 18:33:30 +0300

    Решение домашнего задания 04

--- src/main/scala/ru/otus/homework/hw04/task_futures_sequence.scala
+++ src/main/scala/ru/otus/homework/hw04/task_futures_sequence.scala
@@ -2,27 +2,26 @@ package ru.otus.homework.hw04
 
 import ru.otus.homework.hw04.HomeworksUtils.task
 
-import scala.::
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
 
 object task_futures_sequence {
 
   /** В данном задании Вам предлагается реализовать функцию fullSequence,
-   * похожую на Future.sequence, но в отличие от нее,
-   * возвращающую все успешные и не успешные результаты.
-   * Возвращаемое тип функции - кортеж из двух списков,
-   * в левом хранятся результаты успешных выполнений,
-   * в правовой результаты неуспешных выполнений.
-   * Не допускается использование методов объекта Await и мутабельных переменных var
-   */
+    * похожую на Future.sequence, но в отличие от нее,
+    * возвращающую все успешные и не успешные результаты.
+    * Возвращаемое тип функции - кортеж из двух списков,
+    * в левом хранятся результаты успешных выполнений,
+    * в правовой результаты неуспешных выполнений.
+    * Не допускается использование методов объекта Await и мутабельных переменных var
+    */
 
   /** @param futures список асинхронных задач
-   * @return асинхронную задачу с кортежом из двух списков
-   */
+    * @return асинхронную задачу с кортежом из двух списков
+    */
   def fullSequence[A](
-                       futures: List[Future[A]]
-                     )(implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] = {
+      futures: List[Future[A]]
+  )(implicit ex: ExecutionContext): Future[(List[A], List[Throwable])] = {
     task"Реализуйте метод `fullSequence`"
     val safeFutures: List[Future[Either[Throwable, A]]] = futures.map { f =>
       f.transform {
@@ -32,12 +31,12 @@ object task_futures_sequence {
     }
     safeFutures
       .foldRight(Future.successful(List.empty[Either[Throwable, A]])) {
-        case (currentFuture, accumulatedFuture) => {
+        case (currentFuture, accumulatedFuture) =>
           for {
             currentVal <- currentFuture
             accVal <- accumulatedFuture
           } yield currentVal :: accVal
-        }
+
       }
       .map { results =>
         (
